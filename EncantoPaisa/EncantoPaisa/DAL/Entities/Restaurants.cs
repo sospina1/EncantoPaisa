14887e696ef9884a14c269cf1d06aae8e66d4021
SEBASTIAN OSPINA GAVIRIA <sebastianospina305330@correo.itm.edu.co>
2024-06-06 18:54:19 -0500

    Creation the Entities and configuration the DataBaseContext

--- EncantoPaisa/EncantoPaisa/DAL/Entities/Restaurants.cs
+++ EncantoPaisa/EncantoPaisa/DAL/Entities/Restaurants.cs
@@ -1,6 +1,6 @@
 ﻿namespace EncantoPaisa.DAL.Entities
 {
-    public class Hotels : AuditBase
+    public class Restaurants : AuditBase
     {
         public string Name { get; set; }
         public string Description { get; set; }
